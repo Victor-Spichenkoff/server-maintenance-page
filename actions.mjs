10c92627c8f90171a4fd8f3e4cca387ef66079f3
Victor Spichenkoff <victorspich13@gmail.com>
2024-01-06 20:15:03 -0300

    Change to production url

--- actions.mjs
+++ actions.mjs
@@ -4,7 +4,8 @@
 const resposta = document.getElementById('resposta')
 const btn = document.getElementById('btn')
 
-const url = 'http://localhost:2006'
+// const url = 'http://localhost:2006'
+const url = 'https://server-maintenance.onrender.com'
 const loadGif = 'https://cdn.pixabay.com/animation/2023/10/08/03/19/03-19-26-213_512.gif'
 
 
@@ -17,7 +18,9 @@ async function testar() {
     img.src = loadGif
 
     resposta.appendChild(img)
-
+    
+    var sec = 0
+    setInterval(()=> console.log(sec++), 1000)
     const res =  await axios(url+'/load')
     const msg = res.data
 
