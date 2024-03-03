6ec1d374efee01fbe91104c28825c9826d77923c
Victor Spichenkoff <victorspich13@gmail.com>
2024-03-03 15:56:10 -0300

    Implemented front from new functions

--- actions.mjs
+++ actions.mjs
@@ -2,30 +2,48 @@
 
 const resposta = document.getElementById('resposta')
 const btn = document.getElementById('btn')
+const btn2 = document.getElementById('btn2')
 const listados = document.querySelectorAll('listados')
 
 
-// const url = 'http://localhost:2006'
+// const url = 'http://localhost:2009'
 // const url = 'https://server-maintenance.onrender.com'//old
 const url = 'https://server-maintenance-ssu7.onrender.com'
 
 const loadGif = 'https://cdn.pixabay.com/animation/2023/10/08/03/19/03-19-26-213_512.gif'
 
 
-carregarStatus()
-
-
 const status = document.getElementById('manter-individual')
+
+
+getStatusAfeterFetch()
+
+
+var tentativas = 0
 async function carregarStatus() {
-
-    const res = await axios.get(url+'/currenton')
-    const msg = res.data
-
-    status.innerText = msg
+    try{
+        status.innerHTML = 'Buscando...'
+        const res = await axios.get(url+'/currenton')
+        const msg = res.data
+        console.log('try: ' + res)
+    
+        status.innerText = msg
+    
+        return msg
+    } catch(e) { 
+        status.innerHTML = 'Não foi obtida resposta'
+        console.log('Erro no statusGetter: '+  e)
+        return false
+        // const timeOutAtual = setTimeout(()=> {
+        //     carregarStatus()
+        //     tentativas++
+        // }, 3000)
+        // if(tentativas > 10) return false//não deu mesmo
+     }
 } 
 
 
-async function testar() {
+async function testar(dois = false) {
 
     resposta.innerHTML = ''
     resposta.classList.remove('personalizar')
@@ -36,9 +54,16 @@ async function testar() {
     resposta.appendChild(img)
     
     var sec = 0
-    setInterval(()=> console.log(sec++), 1000)
-    const res =  await axios(url+'/load')
+    const tempoDecorrido = setInterval(()=> {
+        sec++, 1000
+    })
+
+    if(dois) var res = await axios(url+'/forceAllOnce')
+    // if(dois) var res = await axios(url+'/initialLoad')
+    else var res =  await axios(url+'/load')
     const msg = res.data
+
+    clearInterval(tempoDecorrido)
 
     resposta.classList.add('personalizar')
 
@@ -49,12 +74,23 @@ async function testar() {
 async function setarUma(nome) {
     const res = await axios.get(url+'/'+nome)
 
-    carregarStatus()
+    // const currenton = await axios.get(url+ '/currenton')
+
+
+    getStatusAfeterFetch()
+}
+
+async function getStatusAfeterFetch() {
+    const res = await carregarStatus()
+
+    if(res) return
+    setTimeout(getStatusAfeterFetch, 2000)
 }
 
 
 
 btn.onclick = testar
+btn2.onclick = () => testar(true)//é o dois(initial load)
 
 const portfolio = document.getElementById('portfolio')
 portfolio.onclick = async () => setarUma('portfolio')
@@ -76,4 +112,34 @@ all.onclick = async () => setarUma('all')
 const desligar = document.getElementById('desligar')
 desligar.onclick = () => {
     axios.get(url+'/turnoff')
+    getStatusAfeterFetch()
 }
+
+
+//varias mensagens area
+const vmRes = document.getElementById('varias-menagens-res')//variasMsgResposta
+
+
+async function getStatusSeveralMenssages() {
+    vmRes.innerHTML = 'Buscando'
+    const interval = setInterval(async ()=>{
+        try{
+            const res = await axios(url+'/hightMenssagesStatus')
+            if(res) clearInterval(interval)
+            vmRes.innerHTML = res.data
+        } catch(e) {
+            getStatusSeveralMenssages()
+        }
+    }, 1000)
+    
+}
+
+getStatusSeveralMenssages()
+
+async function toggleSeveralMenssages() {
+    vmRes.innerHTML = 'Buscando'
+    const res = await axios(url + '/toggleHightMenssages')
+    vmRes.innerHTML = res.data
+}
+
+document.getElementById('msg-constantes').onclick = toggleSeveralMenssages
